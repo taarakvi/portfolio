521f4a33107357f689e421ea22b90af7b649f293
taarakvi <taarakvi777@gmail.com>
2025-12-16 23:29:30 +0530

    portfolio

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ bar.addEventListener("click",()=>{
     // barOpt.style.display = "block"
 
 })
+
+
+function eduF(){
+    let eduB = document.querySelector(".educations")
+    let sect1 = document.querySelector(".container")
+    let sect2 = document.querySelector(".skillContainer")
+    let sect3 = document.querySelector("section3")
+    // eduB.style.display = "none"
+    eduB.addEventListener("click",()=>{
+        sect3.style.display="block"
+        sect1.style.display="none"
+        sect2.style.display="none"
+    })
+    
+}eduF();
