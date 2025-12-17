d13b0e37ddfe26c6e2aa99a753a10d821e682d16
taarakvi <taarakvi777@gmail.com>
2025-12-17 11:00:51 +0530

    portfolio

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ function eduF(){
     })
     
 }eduF();
+function eduF(){
+    let eduB = document.querySelector(".H-educations")
+    let sect1 = document.querySelector(".container")
+    let sect2 = document.querySelector(".skillContainer")
+    let sect3 = document.querySelector("section3")
+    // eduB.style.display = "none"
+    eduB.addEventListener("click",()=>{
+        sect3.style.display="block"
+        sect1.style.display="none"
+        sect2.style.display="none"
+    })
+    
+}eduF();
